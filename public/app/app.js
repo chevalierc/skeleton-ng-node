fba7ca9b8749ae2846b0abdee425076587380669
Chevalier <christopher.chevalier@pfizer.com>
2016-11-01 14:11:47 -0400

    init

--- public/app/app.js
+++ public/app/app.js
@@ -6,11 +6,3 @@ angular.module( 'app', [ 'ngMaterial',
     'app.routes',
     'mainCtrl'
 ] )
-
-// // application configuration to integrate token into requests
-.config( function( $httpProvider ) {
-
-    // attach our auth interceptor to the http requests
-    $httpProvider.interceptors.push( 'AuthInterceptor' );
-
-} )
